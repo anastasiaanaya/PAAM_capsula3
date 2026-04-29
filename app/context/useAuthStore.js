1cc10d5dd99bca0dddb0bc9346f3abca07f47911
anastasiaanaya <154979891+anastasiaanaya@users.noreply.github.com>
2026-04-29 20:10:44 +0200

    zustand
    ha fet login? user té dades -> Passa cap al xat (app).
    No estàs loguejat? -> Cap a la pantalla de login (auth)

---
+++ app/context/useAuthStore.js
@@ -0,0 +1,22 @@
+import { clear } from 'react-native/types_generated/Libraries/LogBox/Data/LogBoxData';
+import { create } from 'zustand';
+
+export const useAuthStore = create((set) => ({
+    //variables globals
+  user: null,
+  session: null,
+  isInitialized: false, //per saber si ja havia iniciat sessió
+
+  //per guardar l'user quan fa login
+  setAuth:(session) => set ({
+    session: session,
+    user: session ? session.user: null,
+    isInitialized: true,
+  }),
+
+  //per esborrar l'user si fa logout
+  clearAuth: () => set ({
+    session: null,
+    user: null
+  }),
+}));
